93879a2f5a0e3bfcbefb34c34fc0fdf843a3fb6f
Sean <chihhsuanlin0901@gmail.com>
2026-04-30 11:27:20 -0700

    Update py_game.py

--- py_game.py
+++ py_game.py
@@ -1,1 +1,8 @@
-print("hello world")
+#This is a pygame program, I'm going to make Megalovania game(Especially the sans boss fight)
+import pygame
+# Initialize Pygame
+pygame.init()
+# Set up the display
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Megalovania Game")
+# Define colors(THere wil be lack background, white text, red for the player(heart), blue and orange for boss atttacks)
