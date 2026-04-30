6486c6f637897292e3b97a9fe4921432746ec671
Sean <chihhsuanlin0901@gmail.com>
2026-04-30 11:43:54 -0700

    Define colors for game display and elements
    Added color definitions for game elements.

--- py_game.py
+++ py_game.py
@@ -6,3 +6,10 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Megalovania Game")
 # Define colors(THere wil be lack background, white text, red for the player(heart), blue and orange for boss atttacks)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+ORANGE = (255, 165, 0)
+# Set up the player properties
+
