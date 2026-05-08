393b56d725c70aac532a32a76910bc5916069836
Sean <chihhsuanlin0901@gmail.com>
2026-05-08 10:45:52 -0700

    Document boss attacks in attacks.py
    Added comments to explain boss attacks in normal mode.

--- attacks.py
+++ attacks.py
@@ -1,2 +1,29 @@
-# This is the boss' attacks 
-
+# This is the boss' attacks, and this is normal mode
+#                         |
+#                         |(bone is at the middle and is going down)
+#                         |
+#                         v
+#              ------------------------ < ------ this is wall
+#              |                      |     
+#              |                      |    (bone is at the middleand is going left)
+#              |          *           |  <---------
+#              |       (player)       |
+#              |                      |
+#              |                      |
+#              ------------------------
+#
+#   (Same as the attack going from the left and the bottom)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
