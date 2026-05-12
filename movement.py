19686fff82ebab1bdf125a46176fc953a60c79f1
Sean <chihhsuanlin0901@gmail.com>
2026-05-12 10:57:34 -0700

    Update and rename movement.py to

--- movement.py
+++ movement.py
@@ -1,14 +1,3 @@
-#This is the movement when the player is in red heart mode
-
-
-square = pygame.Rect(
-    (800 - square_size) // 2,
-    (600 - square_size) // 2,
-    square_size,
-    square_size,
-)
-square_x = float(square.x)
-square_y = float(square.y)
 
 
 
@@ -47,6 +36,3 @@ square_y = float(square.y)
 
 
 
-
-
-#This is the movement when the player is in blue heart mode
